4f7660903c0fb638bd160f7006d82a755a73d5b0
YingyuWu <yywucs@gmail.com>
2015-11-13 21:34:58 -0500

    m

---
+++ js/grid.js
@@ -0,0 +1,64 @@
+var top = [30.567409,118.708782];
+var bottom = [29.189582,118.708782];
+var left = [30.272152,118.344787];
+var right = [30.272152,120.706504];
+
+
+/**
+ * Returns the distance from 'this' point to destination point (using haversine formula).
+ *
+ * @param   {LatLon} point - Latitude/longitude of destination point.
+ * @param   {number} [radius=6371e3] - (Mean) radius of earth (defaults to radius in metres).
+ * @returns {number} Distance between this point and destination point, in same units as radius.
+ *
+ * @example
+ *     var p1 = new LatLon(52.205, 0.119), p2 = new LatLon(48.857, 2.351);
+ *     var d = p1.distanceTo(p2); // Number(d.toPrecision(4)): 404300
+ */
+function distance(lat1,lat2,lon1,lon2){
+	var R = 6371000; // metres
+	var φ1 = lat1.toRadians();
+	var φ2 = lat2.toRadians();
+	var Δφ = (lat2-lat1).toRadians();
+	var Δλ = (lon2-lon1).toRadians();
+
+	var a = Math.sin(Δφ/2) * Math.sin(Δφ/2) +
+	        Math.cos(φ1) * Math.cos(φ2) *
+	        Math.sin(Δλ/2) * Math.sin(Δλ/2);
+	var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a));
+
+	var d = R * c;
+}
+
+/**
+ * Returns the destination point from 'this' point having travelled the given distance on the
+ * given initial bearing (bearing normally varies around path followed).
+ *
+ * @param   {number} distance - Distance travelled, in same units as earth radius (default: metres).
+ * @param   {number} bearing - Initial bearing in degrees from north.
+ * @param   {number} [radius=6371e3] - (Mean) radius of earth (defaults to radius in metres).
+ * @returns {LatLon} Destination point.
+ *
+ * @example
+ *     var p1 = new LatLon(51.4778, -0.0015);
+ *     var p2 = p1.destinationPoint(7794, 300.7); // p2.toString(): 51.5135°N, 000.0983°W
+ */
+function destinationPoint = function(distance, bearing, radius) {
+    radius = (radius === undefined) ? 6371e3 : Number(radius);
+
+    // see http://williams.best.vwh.net/avform.htm#LL
+
+    var δ = Number(distance) / radius; // angular distance in radians
+    var θ = Number(bearing).toRadians();
+
+    var φ1 = this.lat.toRadians();
+    var λ1 = this.lon.toRadians();
+
+    var φ2 = Math.asin( Math.sin(φ1)*Math.cos(δ) +
+                        Math.cos(φ1)*Math.sin(δ)*Math.cos(θ) );
+    var λ2 = λ1 + Math.atan2(Math.sin(θ)*Math.sin(δ)*Math.cos(φ1),
+                             Math.cos(δ)-Math.sin(φ1)*Math.sin(φ2));
+    λ2 = (λ2+3*Math.PI) % (2*Math.PI) - Math.PI; // normalise to -180..+180°
+
+    return new LatLon(φ2.toDegrees(), λ2.toDegrees());
+};
